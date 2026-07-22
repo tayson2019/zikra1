d47b5c91a5be0069de1a98bd33ee865198ad5afb
tayson2019 <tayson.rofan2019@gmail.com>
2026-07-22 17:47:31 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil((async () => {
+  const keys = await caches.keys();
+  await Promise.all(keys.map(key => caches.delete(key)));
+  await self.clients.claim();
+})()));
+self.addEventListener('fetch', () => {});
